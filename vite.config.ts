a0a606f90914f657541f11ad3b8065b3a7b54b7c
redstonekasi <kasi@hanneskann.de>
2023-06-20 20:54:00 +0200

    deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base: "/web/",
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
